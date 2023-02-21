f08c1fe4c513a7fe5c18c58bff4c88082c1557ef
Gui <guilherme.g.braz@outlook.com>
2023-02-20 22:03:30 -0300

    refactor listar usuario

--- src/main/java/com/stefanini/Core/usecase/usuario/listarTodosUsuarios/ListarTodosUsuarios.java
+++ src/main/java/com/stefanini/Core/usecase/usuario/listarTodosUsuarios/ListarTodosUsuarios.java
@@ -1,5 +1,6 @@
 package com.stefanini.Core.usecase.usuario.listarTodosUsuarios;
 
+import com.stefanini.Core.repositories.UsuarioRepository;
 import com.stefanini.Core.usecase.usuario.mappers.UsuarioToExibirUsuarioDto;
 import com.stefanini.Core.usecase.usuario.view.ExibirUsuarioDto;
 import com.stefanini.Dataproviders.Jpa.repository.UsuarioJpaRepository;
@@ -8,16 +9,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListarTodosUsuarios {
-    private final UsuarioJpaRepository usuarioJpaRepository;
+    private final UsuarioRepository usuarioRepository;
     private final UsuarioToExibirUsuarioDto usuarioToExibirUsuarioDto;
 
     public ListarTodosUsuarios(UsuarioJpaRepository usuarioJpaRepository, UsuarioToExibirUsuarioDto usuarioToExibirUsuarioDto) {
-        this.usuarioJpaRepository = usuarioJpaRepository;
+        this.usuarioRepository = usuarioJpaRepository;
         this.usuarioToExibirUsuarioDto = usuarioToExibirUsuarioDto;
     }
 
     public List<ExibirUsuarioDto> execute() {
-        var usuarios = this.usuarioJpaRepository.listarTodos();
+        var usuarios = this.usuarioRepository.listarTodos();
         List<ExibirUsuarioDto> dtoUsuarios = new ArrayList<>();
         usuarios.forEach(usuario -> dtoUsuarios.add(this.usuarioToExibirUsuarioDto.execute(usuario)));
 
